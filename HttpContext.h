c8579e7dfcb112c76ccc3c7aba2238fe90cacb19
乔英杰.nick <qiaoyingjie.nick@shengqugames.com>
2019-09-09 15:33:43 +0800

    commit

---
+++ HttpContext.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include "HttpRequest.h"
+
+class Buffer;
+
+class HttpContext 
+{
+ public:
+  enum HttpRequestParseState
+  {
+    kExpectRequestLine,
+    kExpectHeaders,
+    kExpectBody,
+    kGotAll,
+  };
+
+  HttpContext()
+    : state_(kExpectRequestLine)
+  {
+  }
+
+  // default copy-ctor, dtor and assignment are fine
+
+  // return false if any error
+  bool parseRequest(Buffer* buf);
+
+  bool gotAll() const
+  { return state_ == kGotAll; }
+
+  void reset()
+  {
+    state_ = kExpectRequestLine;
+    HttpRequest dummy;
+    request_.swap(dummy);
+  }
+
+  const HttpRequest& request() const
+  { return request_; }
+
+  HttpRequest& request()
+  { return request_; }
+
+ private:
+  bool parseRequestLine(const char* begin, const char* end);
+
+  HttpRequestParseState state_;
+  HttpRequest request_;
+};
+
---
+++ asio.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<asio.hpp>
+
+
+using namespace asio;
+
+io_service service;
+ip::tcp::endpoint ep (ip::tcp::v4(),8080);
+ip::tcp::acceptor acc(service, ep);
+char buf[1024];
+
+
+void doRead(std::shared_ptr<ip::tcp::socket> sock);
+
+void doWrite(std::shared_ptr<ip::tcp::socket> sock){
+    sock->async_write_some(buffer(buf),[sock](const asio::error_code &err, size_t nwrite){
+        doRead(sock);
+    });
+} 
+void doRead(std::shared_ptr<ip::tcp::socket> sock){
+    sock->async_read_some(buffer(buf),[sock](const asio::error_code &err, size_t nread){
+        std::cout<<buf;
+        doWrite(sock);
+    });
+}
+
+void start_accept(std::shared_ptr<ip::tcp::socket> sock){
+    acc.async_accept(*sock, [sock](const asio::error_code &err){
+        doRead(sock);
+        auto sock = std::make_shared<ip::tcp::socket>(service);
+        start_accept(sock);
+    });
+}
+
+int main(){
+    auto sock = std::make_shared<ip::tcp::socket>(service);
+    start_accept(sock);
+    service.post([]{std::cout<<"hello"<<std::endl;});
+    service.run();
+    return 0;
+}
