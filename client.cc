321e320900c564fc1306a56efa891a5d4361d2d9
Nickqiaoo <nickqiaoo@gmail.com>
2019-09-18 23:38:25 +0800

    commit test

--- client.cc
+++ client.cc
@@ -8,12 +8,13 @@ int main(){
     asio::io_service service;
     asio::ip::tcp::endpoint ep(asio::ip::address::from_string("127.0.0.1"),8080);
     asio::ip::tcp::socket socket(service);
-    
+
     socket.connect(ep);
     Buffer writebuf;
     writebuf.append("GET /hello HTTP/1.1\r\n\r\n",23);
     size_t size = socket.write_some(asio::buffer(writebuf.peek(),writebuf.readableBytes()));
     writebuf.retrieve(size);
+    
     Buffer readbuf;
     size = socket.read_some(asio::buffer(readbuf.beginWrite(),readbuf.writableBytes()));
     readbuf.hasWritten(size);
